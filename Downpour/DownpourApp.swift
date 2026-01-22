276bbbb84473ba8d382fc4bb5ad8429029dc09c9
aa <oneone@gmail.com>
2026-01-22 05:19:34 -0800

    1

---
+++ Downpour/DownpourApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownpourApp.swift
+//  Downpour
+//
+//  Created by aa on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct DownpourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
